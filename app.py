002d6c63d4123a80820cf9f4f93df9038079fac5
Vivek Vijayan <vivekvijayan6282@gmail.com>
2024-03-29 16:24:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import utils as ut
 
+import matplotlib.pyplot as plt
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -38,6 +40,11 @@ def home():
         hop_length_pyin = len(midi_pyin) // 8
         meta_pyin = ut.dtw(collection, midi_pyin, win_length_pyin, hop_length_pyin)
 
-        return render_template('results.html', meta_stft=meta_stft, meta_pyin=meta_pyin)
+        plt.pcolormesh(stft.T)
+        plt.plot(f0_stft * n_fft / sr, c='g')
+        plt.plot(f0_pyin * n_fft / sr, c='r')
+        plt.savefig('plot.png')
+
+        return render_template('output.html', meta_stft=meta_stft, meta_pyin=meta_pyin)
     else:
-        return render_template('index.html')
+        return render_template('input.html')
